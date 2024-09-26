85d85330b7eaa84011f121e33383354e076534ba
Felipe Soares <felipe0103@hotmail.com.br>
2024-09-25 21:53:42 -0300

    sistema de login funcionando

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,36 +2,34 @@ class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token
 
     def after_sign_in_path_for(resource)
-        # Desloga todos os usuários, exceto o usuário atual
-        sign_out_all_users_except(resource)
+      # Desloga todos os usuários, exceto o usuário atual
+      sign_out_all_users_except(resource)
 
-        # Redireciona baseado no tipo de recurso
-        if resource.is_a?(Admin)
-            puts 'Um admin logou emmmmmmmmmm'
-            main_screen_path  # Redireciona para o admin
-        elsif resource.is_a?(User)
-            puts 'Um usuário logou emmmmmmmm'
-            aplicar_path  # Redireciona para o usuário
-        else
-            puts 'O superman chegouooooooooooooooooo'
-            super  # Chama o método da superclasse Devise::SessionsController
-        end
+      # Redireciona baseado no tipo de recurso
+      if resource.is_a?(Admin)
+        puts 'Um admin logou emmmmmmmmmm'
+        main_screen_path  # Redireciona para o admin
+      elsif resource.is_a?(User)
+        puts 'Um usuário logou emmmmmmmm'
+        aplicar_path  # Redireciona para o usuário
+      else
+        puts 'O superman chegouooooooooooooooooo'
+        super  # Chama o método da superclasse Devise::SessionsController
+      end
     end
 
     private
 
     # Desloga todos os usuários autenticados, exceto o usuário fornecido
     def sign_out_all_users_except(current_user)
-    if user_signed_in? && current_user.is_a?(User)
-        sign_out(:user) unless current_user == current_user
-        sign_out(:admin) unless current_user == current_user
-    end
+      # Desloga o admin se o atual usuário for um User
+      if current_user.is_a?(User)
+        sign_out(:admin) if admin_signed_in?
+      end
 
-    if admin_signed_in? && current_user.is_a?(Admin)
-        sign_out(:admin) unless current_user == current_user
-        sign_out(:user) unless current_user == current_user
+      # Desloga o user se o atual usuário for um Admin
+      if current_user.is_a?(Admin)
+        sign_out(:user) if user_signed_in?
+      end
     end
-    end
-
-
-end
+  end
