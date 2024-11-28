4868659f27b983eefe1cc3e672e7dd20ab097c07
Felipe Soares <felipe0103@hotmail.com.br>
2024-11-27 21:52:05 -0300

    gráficos funcionando 100 % e mudança para grafico de radar

--- app/controllers/visualizar_controller.rb
+++ app/controllers/visualizar_controller.rb
@@ -31,24 +31,75 @@ class VisualizarController < ApplicationController
 
         modelo_aplicado = ModeloAplicado.find(@modelo)
         maturidade = Maturidade.find(modelo_aplicado.maturidade_id)
-        vetor_resultado = []
-        menor_contador = maturidade.maiorNivel.to_i
+        
+        # Determina se é alfanumérico ou numérico
+        is_alpha = maturidade.menorNivel.to_s.match?(/[A-Za-z]/)
+        
+        if is_alpha
+            # Converte letras para números (A=1, B=2, etc) para facilitar comparação
+            nivel_atual = maturidade.menorNivel.upcase.ord - 64  # G = 7
+            maior_nivel = maturidade.maiorNivel.upcase.ord - 64  # A = 1
+            niveis = (maior_nivel..nivel_atual).to_a.reverse # [7,6,5,4,3,2,1] (G->A)
+        else
+            nivel_atual = maturidade.menorNivel.to_i
+            maior_nivel = maturidade.maiorNivel.to_i
+            niveis = (nivel_atual..maior_nivel).to_a # Do menor para o maior
+        end
+
+        Rails.logger.debug "\n=============================================="
+        Rails.logger.debug "=== INICIANDO AVALIAÇÃO DE NÍVEL DE MATURIDADE ==="
+        Rails.logger.debug "=============================================="
+        Rails.logger.debug "Modelo ID: #{@modelo}"
+        Rails.logger.debug "Maturidade ID: #{maturidade.id}"
+        Rails.logger.debug "Nível Escolha: #{maturidade.nivelEscolha}"
+        Rails.logger.debug "Menor Nível: #{maturidade.menorNivel} (#{nivel_atual})"
+        Rails.logger.debug "Maior Nível: #{maturidade.maiorNivel} (#{maior_nivel})"
+        Rails.logger.debug "É alfanumérico? #{is_alpha}"
+        Rails.logger.debug "Níveis a avaliar: #{niveis.map { |n| is_alpha ? (n + 64).chr : n }.join(', ')}"
+        Rails.logger.debug "Quantidade de níveis: #{niveis.count}"
+        Rails.logger.debug "Array de níveis: #{niveis.inspect}"
+        Rails.logger.debug "=============================================="
+
+        Rails.logger.debug "\n=== VERIFICANDO DADOS INICIAIS ==="
+        Rails.logger.debug "Dimensões disponíveis: #{@dimensaos.count}"
+        Rails.logger.debug "Processos disponíveis: #{@processos.count}"
+        Rails.logger.debug "Resultados disponíveis: #{@resultados.count if defined?(@resultados)}"
+        Rails.logger.debug "Opção selecionada (@opcao): #{@opcao}"
+        Rails.logger.debug "=============================================="
+
+        nivel_resultado = nivel_atual # Começa com o menor nível
+        ultimo_nivel_avaliado = nil
+        algum_nivel_avaliado = false
+        resumo_docs = {}
 
         # Itera sobre os níveis do menor ao maior
-        while menor_contador >= maturidade.menorNivel.to_i
+        niveis.each do |nivel_num|
+            nivel = is_alpha ? (nivel_num + 64).chr : nivel_num.to_s
+            Rails.logger.debug "\n------------------------------------------"
+            Rails.logger.debug "AVALIANDO NÍVEL: #{nivel} (#{nivel_num})"
+            Rails.logger.debug "Nível resultado atual: #{is_alpha ? (nivel_resultado + 64).chr : nivel_resultado}"
+            Rails.logger.debug "Último nível avaliado: #{ultimo_nivel_avaliado ? (is_alpha ? (ultimo_nivel_avaliado + 64).chr : ultimo_nivel_avaliado) : 'nenhum'}"
+            
+            docs_nivel = []
+            
             case maturidade.nivelEscolha
             when "2" # Resultados
+                Rails.logger.debug "Modo de avaliação: Resultados"
                 @dimensaos.each do |dimensao|
-                    if dimensao.maturidade_id == @opcao
-                        @processos.each do |processo|
-                            if dimensao.id == processo.dimensao_id
-                                @resultados.each do |resultado|
-                                    if processo.id == resultado.processo_id && resultado.nivel_selecionado.to_i == menor_contador
-                                        resultado.docs.each do |doc|
-                                            if doc.modelo == @modelo && (doc.classificacao == 3 || doc.classificacao == 2)
-                                                vetor_resultado.push(menor_contador-1)
-                                            end
-                                        end
+                    next unless dimensao.maturidade_id == @opcao
+                    Rails.logger.debug "  Verificando dimensão #{dimensao.id}"
+                    
+                    @processos.each do |processo|
+                        next unless dimensao.id == processo.dimensao_id
+                        Rails.logger.debug "    Verificando processo #{processo.id}"
+                        
+                        @resultados.each do |resultado|
+                            if processo.id == resultado.processo_id && resultado.nivel_selecionado.to_s == nivel.to_s
+                                Rails.logger.debug "      Encontrado resultado com nível #{resultado.nivel_selecionado}"
+                                resultado.docs.each do |doc|
+                                    if doc.modelo == @modelo
+                                        docs_nivel << doc
+                                        Rails.logger.debug "        Documento encontrado: #{doc.id} (classificação: #{doc.classificacao})"
                                     end
                                 end
                             end
@@ -56,57 +107,109 @@ class VisualizarController < ApplicationController
                     end
                 end
             when "1" # Processos
+                Rails.logger.debug "Modo de avaliação: Processos"
                 @dimensaos.each do |dimensao|
-                    if dimensao.maturidade_id == @opcao
-                        @processos.each do |processo|
-                            if dimensao.id == processo.dimensao_id && processo.nivel_selecionado.to_i == menor_contador
-                                processo.docs.each do |doc|
-                                    if doc.modelo == @modelo && (doc.classificacao == 3 || doc.classificacao == 2)
-                                        vetor_resultado.push(menor_contador-1)
-                                    end
+                    next unless dimensao.maturidade_id == @opcao
+                    Rails.logger.debug "  Verificando dimensão #{dimensao.id}"
+                    
+                    @processos.each do |processo|
+                        if dimensao.id == processo.dimensao_id && processo.nivel_selecionado.to_s == nivel.to_s
+                            Rails.logger.debug "    Encontrado processo com nível #{processo.nivel_selecionado}"
+                            processo.docs.each do |doc|
+                                if doc.modelo == @modelo
+                                    docs_nivel << doc
+                                    Rails.logger.debug "      Documento encontrado: #{doc.id} (classificação: #{doc.classificacao})"
                                 end
                             end
                         end
                     end
                 end
             when "3" # Dimensões
+                Rails.logger.debug "Modo de avaliação: Dimensões"
                 @dimensaos.each do |dimensao|
-                    if dimensao.maturidade_id == @opcao
-                        @processos.each do |processo|
-                            if dimensao.id == processo.dimensao_id && processo.nivel_selecionado.to_i == menor_contador
-                                processo.docs.each do |doc|
-                                    if doc.modelo == @modelo && (doc.classificacao == 3 || doc.classificacao == 2)
-                                        vetor_resultado.push(menor_contador-1)
-                                    end
+                    next unless dimensao.maturidade_id == @opcao
+                    Rails.logger.debug "  Verificando dimensão #{dimensao.id}"
+                    
+                    @processos.each do |processo|
+                        if dimensao.id == processo.dimensao_id && processo.nivel_selecionado.to_s == nivel.to_s
+                            Rails.logger.debug "    Encontrado processo com nível #{processo.nivel_selecionado}"
+                            processo.docs.each do |doc|
+                                if doc.modelo == @modelo
+                                    docs_nivel << doc
+                                    Rails.logger.debug "      Documento encontrado: #{doc.id} (classificação: #{doc.classificacao})"
                                 end
                             end
                         end
                     end
                 end
             end
-            menor_contador -= 1
-        end
 
-        # Calcula o nível final
-        if vetor_resultado.empty?
-            nivel = maturidade.menorNivel
-        else
-            if maturidade.menorNivel.to_s < maturidade.maiorNivel.to_s
-                if maturidade.menorNivel.to_s > vetor_resultado[0].to_s
-                    nivel = maturidade.menorNivel
-                else
-                    nivel = vetor_resultado[0]
-                end
+            docs_avaliados = docs_nivel.select { |doc| [1, 2, 3].include?(doc.classificacao) }
+            resumo_docs[nivel] = {
+                total: docs_nivel.count,
+                avaliados: docs_avaliados.count,
+                classificacoes: docs_avaliados.map(&:classificacao)
+            }
+
+            Rails.logger.debug "Documentos encontrados: #{docs_nivel.count}"
+            
+            # Se não houver documentos neste nível, mantém o nível anterior e para a avaliação
+            if docs_nivel.empty?
+                Rails.logger.debug "=> Nenhum documento encontrado neste nível"
+                Rails.logger.debug "=> Parando avaliação e mantendo nível anterior: #{is_alpha ? (nivel_resultado + 64).chr : nivel_resultado}"
+                break
+            end
+
+            # Verifica se há documentos avaliados (classificação 1, 2 ou 3)
+            if docs_avaliados.empty?
+                Rails.logger.debug "=> Nenhum documento avaliado neste nível"
+                Rails.logger.debug "=> Parando avaliação e mantendo nível anterior: #{is_alpha ? (nivel_resultado + 64).chr : nivel_resultado}"
+                break
+            end
+
+            algum_nivel_avaliado = true
+            ultimo_nivel_avaliado = nivel_num
+            
+            # Verifica se todos os documentos deste nível atendem totalmente
+            todos_atendem = docs_avaliados.all? { |doc| doc.classificacao == 1 }
+            Rails.logger.debug "Documentos avaliados: #{docs_avaliados.count}"
+            Rails.logger.debug "Classificações: #{docs_avaliados.map(&:classificacao).join(', ')}"
+            Rails.logger.debug "Todos os documentos atendem? #{todos_atendem}"
+
+            if todos_atendem
+                # Se todos atendem, atualiza o nível atual
+                nivel_resultado = nivel_num
+                Rails.logger.debug "=> SUCESSO: Todos documentos atendem!"
+                Rails.logger.debug "=> Atualizando nível para: #{is_alpha ? (nivel_resultado + 64).chr : nivel_resultado}"
             else
-                if maturidade.menorNivel.to_s < vetor_resultado[0].to_s
-                    nivel = maturidade.menorNivel
-                else
-                    nivel = vetor_resultado[0]
-                end
+                # Se encontrar algum que não atende, mantém o nível anterior
+                Rails.logger.debug "=> ATENÇÃO: Nem todos os documentos atendem"
+                Rails.logger.debug "=> Parando avaliação e mantendo nível anterior: #{is_alpha ? (nivel_resultado + 64).chr : nivel_resultado}"
+                break
             end
         end
 
-        nivel
+        Rails.logger.debug "\n=============================================="
+        Rails.logger.debug "=== RESULTADO FINAL ==="
+        Rails.logger.debug "Nível final numérico: #{nivel_resultado}"
+        Rails.logger.debug "Algum nível foi avaliado? #{algum_nivel_avaliado}"
+        
+        # Se for alfanumérico, converte o número de volta para letra
+        if is_alpha
+            nivel_resultado = (nivel_resultado + 64).chr
+            Rails.logger.debug "Nível final convertido: #{nivel_resultado}"
+        end
+        
+        Rails.logger.debug "\n=== RESUMO DE DOCUMENTOS POR NÍVEL ==="
+        resumo_docs.each do |nivel, info|
+            Rails.logger.debug "Nível #{nivel}:"
+            Rails.logger.debug "  - Total de documentos: #{info[:total]}"
+            Rails.logger.debug "  - Documentos avaliados: #{info[:avaliados]}"
+            Rails.logger.debug "  - Classificações: #{info[:classificacoes].join(', ')}"
+        end
+        Rails.logger.debug "=============================================="
+        
+        nivel_resultado
     end
 
     def prepare_chart_data
@@ -134,12 +237,28 @@ class VisualizarController < ApplicationController
             end
         end
 
-        # Prepara dados para o gráfico
+        # Calcula o total de documentos
+        total_docs = @nao_atende + @atende_parcialmente + @atende_totalmente + @nao_classificado
+
+        # Calcula as porcentagens
+        porcentagem_nao_atende = total_docs > 0 ? (@nao_atende.to_f / total_docs * 100).round(1) : 0
+        porcentagem_atende_parcialmente = total_docs > 0 ? (@atende_parcialmente.to_f / total_docs * 100).round(1) : 0
+        porcentagem_atende_totalmente = total_docs > 0 ? (@atende_totalmente.to_f / total_docs * 100).round(1) : 0
+        porcentagem_nao_classificado = total_docs > 0 ? (@nao_classificado.to_f / total_docs * 100).round(1) : 0
+
+        # Prepara dados para o gráfico de radar
         @chart_data = {
             labels: ['Não Atende', 'Atende Parcialmente', 'Atende Totalmente', 'Não Classificado'],
             datasets: [{
-                data: [@nao_atende, @atende_parcialmente, @atende_totalmente, @nao_classificado],
-                backgroundColor: ['#dc3545', '#ffc107', '#28a745', '#6c757d']
+                label: 'Distribuição de Classificações (%)',
+                data: [porcentagem_nao_atende, porcentagem_atende_parcialmente, porcentagem_atende_totalmente, porcentagem_nao_classificado],
+                fill: true,
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgb(54, 162, 235)',
+                pointBackgroundColor: 'rgb(54, 162, 235)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgb(54, 162, 235)'
             }]
         }
     end
